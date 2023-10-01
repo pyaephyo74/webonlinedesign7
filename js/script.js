87a22428ee39811729e5110c32f0274f28728dbe
pyaephyo74 <alternative.phyo@gmail.com>
2023-10-01 20:37:18 +0630

    my learn web design

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// let bar = document.querySelector("#bars");
+// let navLinks = document.querySelector(".navlink");
+// bar.addEventListener("click",function(){
+//   //   alert("hi");
+//   navLinks.classList.toggle("show");
+//   bar.classList.toggle("fa-times");
+// });
+// window.addEventListener("scroll",function(){
+//     navLinks.classList.remove("show");
+//     bar.classList.remove("fa-times");
+
+// });
+
+$(document).ready(function(){
+
+
+
+  $('.fa-bars').click(function () { 
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('load scroll',function(){
+    $('.fa-bars').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+  });
+
+  if($(window).scrollTop() > 30){
+    $('.header').css({'background':'#0C8C7A','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+  }else{
+    $('.header').css({'background':'none','box-shadow':'none'});
+
+  }
+});
+$('.accordion-header').click(function(){
+  $('.accordion .accordion-body').slideUp();
+  $(this).next('.accordion-body').slideDown();
+  $('.accordion .accordion-header span').text('+');
+  $(this).children('span').text('-');
+});
